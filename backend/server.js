4599610d4a35eb1a70154dce9de8f9accffbe30d
Pawan Nagar <pawannagar2243@gmail.com>
2026-04-13 17:40:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,14 @@ app.use(express.json());
 
 // 🔥 Email transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4, // 🔥 IMPORTANT: force IPv4
 });
 
 transporter.verify(function (error, success) {
